8156754deaedb07a04e3eff8d647753d8fa99a20
hando1233 <19dh111160@st.huflit.edu.vn>
2022-04-01 00:58:33 +0700

    test commit

---
+++ app/src/main/java/com/example/pregbe/Adapter/SoTuanAdapter.java
@@ -0,0 +1,13 @@
+package com.example.pregbe.Adapter;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SoTuanAdapter  {
+
+}
---
+++ app/src/main/java/com/example/pregbe/GioiThieu/ChaoMung2.java
@@ -0,0 +1,16 @@
+package com.example.pregbe.GioiThieu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pregbe.R;
+
+public class ChaoMung2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chao_mung2);
+    }
+}
